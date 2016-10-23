42cbb43ac78fe8c105262eb33f4a85d9bd26ca9a
Ryo Murakami <integral14@gmail.com>
2016-10-23 12:06:46 +0900

    hours larger than 24

--- dmmeng2gcal.js
+++ dmmeng2gcal.js
@@ -1,33 +1,51 @@
+// Parse date string to date objects
+var parseDate = function(text) {
+  var splited = text.split(')');
+  var date_text = splited[0].split('(')[0].replace('年', '/').replace('月', '/').replace('日', '');
+
+  var times = splited[1].split(' - ');
+
+  var dates = [];
+
+  // time sanitization
+  // if hours exceeds 24, shift 1 day later
+  times.forEach(function(t) {
+    var shift = 0;
+    var hours = parseInt(t.split(':')[0]);
+    var mins = parseInt(t.split(':')[1]);
+    if (hours >= 24) {
+      shift = 1;
+      hours -= 24;
+    }
+
+    var date = new Date(
+      date_text + ' ' + hours + ':' + mins);
+    date.setDate(date.getDate() + shift)
+    dates.push(date);
+  });
+  return dates;
+}
+
+// Date object to string for google calendar
+var dateToString = function(date) {
+  return date.getFullYear() +
+    ('00' + date.getMonth()).substr(-2) +
+    ('00' + date.getDate()).substr(-2) +
+    'T' +
+    ('00' + date.getHours()).substr(-2) +
+    ('00' + date.getMinutes()).substr(-2) +
+    '00';
+};
+
 var elem = document.getElementsByClassName('l_time')[0];
-var text = elem.textContent;
-
-var splited;
-
-splited  = text.split('年');
-var year = splited[0];
-text = splited[1];
-
-splited = text.split('月');
-var month = splited[0];
-text = splited[1];
-
-splited = text.split('日(');
-var day = splited[0];
-text = splited[1];
-
-splited = text.split(')')[1].split(' - ');
-var t0 = splited[0].replace(':', '') + '00';
-var t1 = splited[1].replace(':', '') + '00';
-
-var day0 = year + month + day;
-var day1 = year + month + day;
-var dates = day0 + 'T' + t0 + '/' + day1 + 'T' + t1;
-
+var dates = parseDate(elem.textContent);
 var teacher_name = document.getElementsByClassName('en_name')[0].textContent;
+
+var dates_text = dates.map(dateToString).join('/');
 
 var title = '【DMM英会話】' + teacher_name;
 var detail = 'http://eikaiwa.dmm.com/book/book_list/';
 
-var href = 'http://www.google.com/calendar/event?action=TEMPLATE&text=' + title + '&dates=' + dates + '&details=' + detail;
+var href = 'http://www.google.com/calendar/event?action=TEMPLATE&text=' + title + '&dates=' + dates_text + '&details=' + detail;
 
 window.open(href);
